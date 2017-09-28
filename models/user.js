8ba4345104fa70a25cd9ea9a50df7ba58f628e9b
Devon JohnBaptiste <devondevon@outlook.com>
2017-09-27 22:05:58 -0400

    passes 3 tests

--- models/user.js
+++ models/user.js
@@ -2,9 +2,44 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
 
 const User = db.define('user', {
-// YOUR CODE HERE...
+  first: {
+    type: Sequelize.STRING
+  },
+  last: {
+    type: Sequelize.STRING
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 23
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    }
+  },
+  bio: {
+    type: Sequelize.TEXT
+  }
 }, {
-// ...AND HERE
+  hooks: {
+    validationFailed: (user, options, err) => {
+      if (err) {
+        if (err.errors[0].path === 'email') {
+          throw new Error('email cannot be null');
+        }
+      }
+    }
+  },
+  getterMethods: {
+    fullName () {
+      return this.first + ' ' + this.last;
+    }
+  }
 });
 
 module.exports = User;
